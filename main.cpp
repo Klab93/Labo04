b8f1b7a2e3d610a5406de908050f431f2762adf5
Carl <Carl@DESKTOP-9PRQRUO>
2020-04-20 14:22:11 +0200

    Test

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Carl
+ *
+ * Created on 20. avril 2020, 14:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   
+   //salut camarades 
+   return 0;
+}
+
