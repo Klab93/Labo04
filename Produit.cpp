9123e5b73e4fb962a416381a09b2dc00157f568f
soso24soso <61196412+soso24soso@users.noreply.github.com>
2020-04-22 14:52:03 +0200

    entête sur produit

--- Produit.cpp
+++ Produit.cpp
@@ -1,7 +1,17 @@
-//
-// Created by AnneSo on 22.04.2020.
-//
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : main.cpp
+ Auteur(s)   : Anne Sophie Ganguillet, Kylian Bourcoud, Carl Penalva
+ Date        : 22.04.2020
 
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
 #include "produit.h"
 
 
