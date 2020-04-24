8c1fb23544332003321476bbeda1353c2a091c45
soso24soso <61196412+soso24soso@users.noreply.github.com>
2020-04-24 12:18:48 +0200

    commentaires classe produit

--- Produit.cpp
+++ Produit.cpp
@@ -1,13 +1,13 @@
 /*
  -----------------------------------------------------------------------------------
  Laboratoire : 04
- Fichier     : main.cpp
+ Fichier     : Produit.cpp
  Auteur(s)   : Anne Sophie Ganguillet, Kylian Bourcoud, Carl Penalva
  Date        : 22.04.2020
 
- But         :
+ But         : implémentation de la classe produit
 
- Remarque(s) :
+ Remarque(s) : -
 
  Compilateur : MinGW-g++ 6.3.0
  -----------------------------------------------------------------------------------
@@ -21,7 +21,7 @@ using namespace std;
 Produit::Produit(int no, const string& libelle, double prix)
    :no(no), libelle(libelle), prix(prix) {
    if(prix < 0.05) throw(PrixNonValide("Erreur dans Produit::Produit :\n"
-                                        "le prix doit etre >= 5 cts !"));
+                                          "le prix doit etre >= 5 cts !"));
 }
 
 ostream &operator<<(ostream &os, const Produit &p) {
@@ -32,7 +32,7 @@ ostream &operator<<(ostream &os, const Produit &p) {
 
 void Produit::setPrix(double _prix) {
    if(_prix < 0.05) throw(PrixNonValide("Erreur dans Produit::setPrix :\n"
-                                     "le prix doit etre >= 5 cts !"));
+                                           "le prix doit etre >= 5 cts !"));
    prix = _prix;
 }
 
